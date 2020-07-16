246f49af499eb2241043ab4589c4e5b479255ae3
georgina puig <georginaxcodes@gmail.com>
2020-07-16 16:51:57 -0500

    connect mongoose

--- embed.js
+++ embed.js
@@ -1,1 +1,9 @@
 const mongoose = require('mongoose'); // npm install mongoose --save
+
+// mongoose setup
+mongoose.connect('mongodb://localhost:27017/blog_demo', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+  .then(() => console.log('Connected to DB!'))
+  .catch(error => console.log(error.message));
