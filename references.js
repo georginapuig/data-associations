795b1e5c7bf901aa49f005cd205c2594f016fe90
georgina puig <georginaxcodes@gmail.com>
2020-07-16 20:21:20 -0500

    fill the post array with all the data

--- references.js
+++ references.js
@@ -33,22 +33,32 @@ const User = mongoose.model("User", userSchema);
 //   name: 'Bob Belcher'
 // });
 
-Post.create({
-  title: 'How to cook the best burguer pt. 3',
-  content: 'lalalala alallala'
-}, function(err, post) {
-  User.findOne({email: 'bob@gmail.com'}, function(err, foundUser) {
-    if (err) {
-      console.log(err);
-    } else {
-      foundUser.posts.push(post);
-      foundUser.save(function(err, data) {
-        if (err) {
-          console.log(err);
-        } else {
-          console.log(data);
-        }
-      });
-    }
-  });
+// Post.create({
+//   title: 'How to cook the best burguer pt. 3',
+//   content: 'lalalala alallala'
+// }, function(err, post) {
+//   User.findOne({email: 'bob@gmail.com'}, function(err, foundUser) {
+//     if (err) {
+//       console.log(err);
+//     } else {
+//       foundUser.posts.push(post);
+//       foundUser.save(function(err, data) {
+//         if (err) {
+//           console.log(err);
+//         } else {
+//           console.log(data);
+//         }
+//       });
+//     }
+//   });
+// });
+
+// find user
+// find all post for that user
+User.findOne({email: 'bob@gmail.com'}).populate('posts').exec(function(err, user) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(user);
+  }
 });
